6af9f07082bf10724d56432baedbc9cbd051d8cd
Adam Menkes <amenkes24@gmail.com>
2021-10-12 14:26:36 -0700

    add ability to reset tasks as well as an alert feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,28 @@ var createTaskHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value
 
-  // create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+// check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+     }
+     formEl.reset();
+
+// create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
 // create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
+    var taskInfoEl = document.createElement("div");
 // give it a class name
-taskInfoEl.className = "task-info";
+    taskInfoEl.className = "task-info";
 // add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
 
-listItemEl.appendChild(taskInfoEl);
+    listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl);
   };
 
 formEl.addEventListener("submit", createTaskHandler);
